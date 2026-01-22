8dbd393db4e96f7c5631cdf2ff957fd65f0afc94
Dimitrius <speed.Dima05@gmail.com>
2026-01-23 00:16:49 +0300

    Finally realised rentals entity

--- app/repositories/strategies/json/rentals_storage.rb
+++ app/repositories/strategies/json/rentals_storage.rb
@@ -1,5 +1,3 @@
-require "active_support/core_ext/hash"
-require "active_support/json"
 require 'ostruct'
   module Strategies
     module Json
@@ -7,7 +5,6 @@ require 'ostruct'
         FILE_PATH = Rails.root.join("backup", "rentals.json")
 
         def all
-          puts '1'
           load_data.map { |attrs| 
             rental = Strategies::Json::Rental.new(attrs)
             rental.client= find_client(attrs["client_id"])
@@ -17,7 +14,6 @@ require 'ostruct'
         end
 
         def find(id)
-          puts '2'
           data = load_data.find { |r| r["id"] == id.to_i }
           rental = Strategies::Json::Rental.new(data)
           rental.client= find_client(data["client_id"])
@@ -26,21 +22,21 @@ require 'ostruct'
         end
 
         def create(attrs)
-          puts '3'
           data = load_data
 
           attrs["id"] = next_id(data)
-          data << attrs
+          attrs = attrs.stringify_keys
+          data << attrs.to_unsafe_h.to_hash
           save_data(data)
           attrs
         end
 
 
         def update(id, attrs)
-          puts '4'
           data = load_data
           rental = data.find { |r| r["id"] == id.to_i }
           return nil unless rental
+
 
           rental.merge!(attrs.stringify_keys)
           save_data(data)
@@ -48,7 +44,6 @@ require 'ostruct'
         end
 
         def destroy(id)
-          puts '5'
           data = load_data
           data.reject! { |r| r["id"] == id.to_i }
           save_data(data)
@@ -66,15 +61,6 @@ require 'ostruct'
           Strategies::Json::Scooter.new(scooter_data)
         end
 
-  
-        def ruby_string_to_hash(str)
-          cleaned = str
-            .gsub("=>", ":")
-            .gsub(/(\w+):/, '"\1":')
-
-          JSON.parse(cleaned)
-        end
-
 
         def load_data
           return [] unless File.exist?(FILE_PATH)
@@ -82,8 +68,6 @@ require 'ostruct'
         end
 
         def save_data(data)
-          puts "SAVING THEM"
-          #puts ruby_string_to_hash(data)
           File.write(FILE_PATH, JSON.pretty_generate(data))
         end
 
